00c71a522ca02f4d641224ae766de9d043f86f9b
hosainahmeed <skyfal430@gmail.com>
2025-10-11 09:34:34 +0600

    update date picker calender

--- src/components/orgnizer-event-list/DatePickerCustom.tsx
+++ src/components/orgnizer-event-list/DatePickerCustom.tsx
@@ -1,14 +1,15 @@
-import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover"
 import { Button } from "../ui/button"
 import { CalendarIcon } from "lucide-react"
 import { format } from "date-fns"
 import { Calendar } from "../ui/calendar"
 import { OnSelectHandler } from "react-day-picker"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+
 
 function DatePickerCustom({ date, setDate }: { date: Date | undefined, setDate: (date: Date | undefined) => void }) {
     return (
         <div className="w-full">
-            {/* <Popover>
+            <Popover>
                 <PopoverTrigger asChild>
                     <Button
                         variant="outline"
@@ -22,13 +23,13 @@ function DatePickerCustom({ date, setDate }: { date: Date | undefined, setDate: 
                 <PopoverContent className="w-auto bg-white p-0">
                     <Calendar mode="single" selected={date} onSelect={setDate as OnSelectHandler<Date | undefined>} />
                 </PopoverContent>
-            </Popover> */}
-            <input
+            </Popover>
+            {/* <input
                 className="w-full shadow rounded border py-3 bg-white px-3"
                 type="date"
                 value={date ? format(date, "yyyy-MM-dd") : ""}
                 onChange={(e) => setDate(new Date(e.target.value))}
-            />
+            /> */}
         </div>
     )
 }
