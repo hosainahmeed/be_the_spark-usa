2899cdd560ee3429f15f38568ad057362ece27f4
hosainahmeed <skyfal430@gmail.com>
2026-01-20 11:19:18 +0600

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,15 @@ export default function RootLayout({
   };
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={optima.className}>
         <ReduxWrapper>
           <RoutingSafe>
